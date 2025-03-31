062758528ef00b72feadc516bcde777a95f14382
Nguyen Huy <huyngh05@gmail.com>
2025-03-31 23:03:35 +0700

    Generate classes including Accommodation, Adress, Policy

--- API/src/classes/Accommodation.js
+++ API/src/classes/Accommodation.js
@@ -2,6 +2,27 @@
  * Accommodation class
  * @class Accommodation
  */
-class Accommodation {}
+class Accommodation {
+	constructor(id, name, amenities, address, rooms, images) {
+		this.id = id;
+		this.name = name;
+		this.amenities = amenities;
+		this.address = address;
+		this.rooms = rooms;
+		this.images = images;
+	}
+
+	getRoomCount() {
+		return this.rooms.length;
+	}
+
+	getAvailableRooms(startDate, endDate) {
+		return this.rooms.filter((room) => room.isAvailable(startDate, endDate));
+	}
+
+	getBookings() {
+		return this.rooms.flatMap((room) => room.getBookings());
+	}
+}
 
 export default Accommodation;
--- API/src/classes/Policy.js
+++ API/src/classes/Policy.js
@@ -2,6 +2,27 @@
  * An accommodation's policy
  * @class Policy
  */
-class Policy {}
+class Policy {
+	static ECancelPolicy = Object.freeze({
+		CANCEL_24H: "CANCEL_24H",
+		CANCEL_3D: "CANCEL_3D",
+		CANCEL_7D: "CANCEL_7D",
+		CANCEL_15D: "CANCEL_15D",
+		NO_CANCEL: "NO_CANCEL",
+	});
+
+	static EPrepayPolicy = Object.freeze({
+		FULL: "FULL",
+		HALF: "HALF",
+	});
+
+	constructor(id, checkinTime, checkoutTime, cancellation, prepay) {
+		this.id = id;
+		this.checkinTime = checkinTime;
+		this.checkoutTime = checkoutTime;
+		this.cancellation = cancellation; // Using Enum ECancelPolicy
+		this.prepay = prepay; // Using Enum EPrepayPolicy
+	}
+}
 
 export default Policy;
