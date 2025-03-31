0fcfa89fce308ea5b3f9d75cb67bf8b769bac9ce
JackieLOLz1 <trumkamatat@gmail.com>
2025-04-01 00:00:07 +0700

    Booking class, Invoice class, Room class

--- API/src/classes/Invoice.js
+++ API/src/classes/Invoice.js
@@ -2,6 +2,15 @@
  * Information about an invoice. Contains 1 or many bookings.
  * @class Invoice
  */
-class Invoice {}
+class Invoice {
+    constructor(id, bookings) {
+        this.id = id;
+        this.bookings = bookings;
+    }
+
+    calculateTotalPrice() {
+        
+    }
+}
 
 export default Invoice;
