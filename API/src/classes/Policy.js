509493c9e094537aec8d9f89be1e77bdf7fa428e
Nguyen Huy <huyngh05@gmail.com>
2025-04-01 10:01:06 +0700

    Declare and export enum outside the Policy class --> Policy.js

--- API/src/classes/Policy.js
+++ API/src/classes/Policy.js
@@ -3,26 +3,36 @@
  * @class Policy
  */
 class Policy {
-	static ECancelPolicy = Object.freeze({
-		CANCEL_24H: "CANCEL_24H",
-		CANCEL_3D: "CANCEL_3D",
-		CANCEL_7D: "CANCEL_7D",
-		CANCEL_15D: "CANCEL_15D",
-		NO_CANCEL: "NO_CANCEL",
-	});
-
-	static EPrepayPolicy = Object.freeze({
-		FULL: "FULL",
-		HALF: "HALF",
-	});
-
 	constructor(id, checkinTime, checkoutTime, cancellation, prepay) {
 		this.id = id;
 		this.checkinTime = checkinTime;
 		this.checkoutTime = checkoutTime;
-		this.cancellation = cancellation; // Using Enum ECancelPolicy
-		this.prepay = prepay; // Using Enum EPrepayPolicy
+		this.cancellation = cancellation; // Expecting value from ECancelPolicy
+		this.prepay = prepay; // Expecting value from EPrepayPolicy
 	}
 }
+
+/**
+ * Enum for cancellation policies.
+ * @readonly
+ * @enum {string}
+ */
+export const ECancelPolicy = Object.freeze({
+	CANCEL_24H: "CANCEL_24H",
+	CANCEL_3D: "CANCEL_3D",
+	CANCEL_7D: "CANCEL_7D",
+	CANCEL_15D: "CANCEL_15D",
+	NO_CANCEL: "NO_CANCEL",
+});
+
+/**
+ * Enum for prepayment policies.
+ * @readonly
+ * @enum {string}
+ */
+export const EPrepayPolicy = Object.freeze({
+	FULL: "FULL",
+	HALF: "HALF",
+});
 
 export default Policy;
