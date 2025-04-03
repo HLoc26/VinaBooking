061053e18651358f0d0e33839034cb1edaf6809b
HLoc26 <dhl26052004@gmail.com>
2025-04-03 07:52:27 +0700

    fix: update navbar position from fixed -> sticky

--- UI/src/components/layout/NavBar/NavBar.jsx
+++ UI/src/components/layout/NavBar/NavBar.jsx
@@ -17,7 +17,7 @@ function Navbar() {
 	};
 
 	return (
-		<AppBar position="fixed" color="primary">
+		<AppBar position="sticky" color="primary">
 			<Toolbar>
 				{/* Logo */}
 				<Typography variant="h6" component="div" sx={{ flexGrow: 1, fontWeight: "bold" }}>
