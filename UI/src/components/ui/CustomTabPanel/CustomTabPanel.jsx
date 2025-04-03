e42e477fa30f3949c7d20237a2c3c7c5a65bcfed
HLoc26 <dhl26052004@gmail.com>
2025-04-03 15:41:51 +0700

    feat: change display style to flex, align items center

--- UI/src/components/ui/CustomTabPanel/CustomTabPanel.jsx
+++ UI/src/components/ui/CustomTabPanel/CustomTabPanel.jsx
@@ -5,7 +5,7 @@ function CustomTabPanel(props) {
 
 	return (
 		<div role="tabpanel" hidden={value !== index} id={`simple-tabpanel-${index}`} aria-labelledby={`simple-tab-${index}`} {...other}>
-			{value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+			{value === index && <Box sx={{ p: 3, display: "flex", alignItems: "center", flexDirection: "column" }}>{children}</Box>}
 		</div>
 	);
 }
