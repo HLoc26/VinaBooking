ab71bb8194236ce5677cf8540471661c5ea1f3bb
HLoc26 <dhl26052004@gmail.com>
2025-04-03 07:56:30 +0700

    feat: add AccommodationDetail route for viewing accommodation details

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -3,14 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Auth/Login/Login";
 import Register from "./pages/Auth/Register/Register";
 import Landing from "./pages/Landing/Landing";
+import AccommodationDetail from "./pages/AccommodationDetail/AccommodationDetail";
 
 function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/" element={<Landing />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/register" element={<Register />} />
+				<Route index element={<Landing />} />
+				<Route path="login" element={<Login />} />
+				<Route path="register" element={<Register />} />
+				<Route path="accommodation">
+					<Route path="detail/:aid" element={<AccommodationDetail />} /> {/* aid: Accommodation id */}
+				</Route>
 			</Routes>
 		</Router>
 	);
