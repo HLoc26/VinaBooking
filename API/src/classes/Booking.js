b1704ecdfe4ab2f80a3af2e5e3ac4c79dda6de27
HLoc26 <dhl26052004@gmail.com>
2025-04-08 16:13:44 +0700

    Refactor Booking class constructor and rename BookingStatus to EBookingStatus

--- API/src/classes/Booking.js
+++ API/src/classes/Booking.js
@@ -3,27 +3,25 @@
  * @class Booking
  */
 class Booking {
-    constructor(id, room, guest, startDate, endDate, guestCount, status) {
-        this.id = id;
-        this.room = room;
-        this.guest = guest;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.guestCount = guestCount;
-        this.status = status;
-    }
+	constructor(id, room, guest, startDate, endDate, guestCount, status) {
+		this.id = id;
+		this.room = room;
+		this.guest = guest;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.guestCount = guestCount;
+		this.status = status;
+	}
 
-    calculatePrice() {
-
-    }
+	calculatePrice() {}
 }
 
-const BookingStatus = {
-    BOOKED: "BOOKED",
-    RESERVED: "RESERVED",
-    CHECKED_IN: "CHECKED_IN",
-    COMPLETED: "COMPLETED",
-    CANCELED: "CANCELED"
-};
+export const EBookingStatus = Object.freeze({
+	BOOKED: "BOOKED",
+	RESERVED: "RESERVED",
+	CHECKED_IN: "CHECKED_IN",
+	COMPLETED: "COMPLETED",
+	CANCELED: "CANCELED",
+});
 
 export default Booking;
