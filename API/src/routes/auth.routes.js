193b15c115208d938688b5376ba1771908778634
Sang-ute <nqs28012004@gmail.com>
2025-04-15 12:27:57 +0700

    feat: create auth route

--- API/src/routes/auth.routes.js
+++ API/src/routes/auth.routes.js
@@ -1,9 +1,16 @@
+// routes/auth.routes.js
 import { Router } from "express";
+import AuthController from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
+// Test route
 authRouter.get("/", (req, res) => {
 	res.json({ success: true, message: "Hello, welcome from auth" });
 });
+
+// OTP routes
+authRouter.post("/otp/request", AuthController.requestOTP);
+authRouter.post("/otp/confirm", AuthController.confirmOTP);
 
 export default authRouter;
