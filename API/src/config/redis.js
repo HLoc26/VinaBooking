9d43f465e7707b24ccfafdb197e88b37caadba7c
Sang-ute <nqs28012004@gmail.com>
2025-04-15 12:28:53 +0700

    feat: create redis config file

---
+++ API/src/config/redis.js
@@ -0,0 +1,11 @@
+// config/redis.js
+import Redis from "ioredis";
+
+const redis = new Redis({
+	host: process.env.REDIS_HOST || "localhost",
+	port: process.env.REDIS_PORT || 6380,
+	password: process.env.REDIS_PASSWORD || "",
+	connectionTimeout: 10000, // 10 seconds
+});
+
+export default redis; // default export
