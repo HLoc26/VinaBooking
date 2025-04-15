7a5e35f4ccf23a71ea9394c3f4de9fc663cee843
Bright <mastereziono1@gmail.com>
2025-04-15 11:43:18 +0700

    update index routes

--- API/src/routes/index.routes.js
+++ API/src/routes/index.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { User } from "../database/models/index.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import authRouter from "./auth.routes.js";
 
 const router = Router();
 
