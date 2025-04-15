cbb797b26551f7f6b11aefed87b9074f854ddb6f
zhwy512 <huyngh05@gmail.com>
2025-04-15 17:46:35 +0700

    Fix service of See accommodation detail API to display full details of an accommodation

--- API/src/services/accommodation.service.js
+++ API/src/services/accommodation.service.js
@@ -3,6 +3,8 @@ import AccommodationAmenity from "../database/models/AccommodationAmenity.model.
 import Address from "../database/models/Address.model.js";
 import Image from "../database/models/Image.model.js";
 import Room from "../database/models/Room.model.js";
+import RoomAmenity from "../database/models/RoomAmenity.model.js";
+import Amenity from "../database/models/Amenity.model.js";
 
 export default {
 	async findById(id) {
@@ -10,20 +12,66 @@ export default {
 			where: { id },
 			include: [
 				{
+					model: AccommodationAmenity,
+					include: [{ model: Amenity }],
+				},
+				{
 					model: Address,
 				},
 				{
-					model: AccommodationAmenity,
-				},
-				{
 					model: Room,
+					include: [
+						{ model: Image },
+						{
+							model: RoomAmenity,
+							include: [{ model: Amenity }],
+						},
+					],
 				},
 				{
 					model: Image,
 				},
 			],
-        });
-        
-		return accommodation ? accommodation.toJSON() : null;
+		});
+
+		if (!accommodation) return null;
+
+		const plain = accommodation.toJSON();
+
+		// Merge Amenity fields into AccommodationAmenity
+		plain.AccommodationAmenities = plain.AccommodationAmenities.map((aa) => {
+			const { Amenity, ...rest } = aa;
+			if (Amenity) {
+				const { id: amenityId, ...amenityFields } = Amenity;
+				return {
+					...rest,
+					...amenityFields,
+					amenityId,
+				};
+			}
+			return aa;
+		});
+
+		// Merge Amenity fields into RoomAmenity
+		plain.Rooms = plain.Rooms.map((room) => {
+			const updatedRoom = { ...room };
+			if (room.RoomAmenities) {
+				updatedRoom.RoomAmenities = room.RoomAmenities.map((ra) => {
+					const { Amenity, ...rest } = ra;
+					if (Amenity) {
+						const { id: amenityId, ...amenityFields } = Amenity;
+						return {
+							...rest,
+							...amenityFields,
+							amenityId,
+						};
+					}
+					return ra;
+				});
+			}
+			return updatedRoom;
+		});
+
+		return plain;
 	},
 };
