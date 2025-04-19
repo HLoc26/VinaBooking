85471e11cf1926a2d5e2c05736e73c87c5717eab
zhwy512 <huyngh05@gmail.com>
2025-04-19 16:27:39 +0700

    Add favouriteRouter and call controller add/remove to favourite

---
+++ API/src/routes/favourite.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import favouriteController from "../controllers/favourite.controller.js";
+
+const favouriteRouter = Router();
+
+// Add or remove a favourite accommodation for favourite list of a user
+favouriteRouter.post("/add", favouriteController.addToFavourite);
+favouriteRouter.delete("/remove", favouriteController.removeFromFavourite);
+
+export default favouriteRouter;
