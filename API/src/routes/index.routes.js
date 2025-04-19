ec282af647f4b03930bb2159732a02a55f329133
zhwy512 <huyngh05@gmail.com>
2025-04-19 16:25:10 +0700

    Add favouriteRouter into index router

--- API/src/routes/index.routes.js
+++ API/src/routes/index.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import accommodationRouter from "./accommodation.routes.js";
 import authRouter from "./auth.routes.js";
+import favouriteRouter from "./favourite.routes.js";
 
 const router = Router();
+
+router.use("/accommodation", accommodationRouter);
+router.use("/favourite", favouriteRouter);
 
 router.get("/", (req, res) => {
 	res.json({ success: true, message: "Hello, welcome" });
