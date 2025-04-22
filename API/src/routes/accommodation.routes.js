dda1889dc124a856bde6f8e37729f44b7d782c21
HLoc26 <dhl26052004@gmail.com>
2025-04-23 01:16:52 +0700

    feat: Implement accommodation search functionality

--- API/src/routes/accommodation.routes.js
+++ API/src/routes/accommodation.routes.js
@@ -3,6 +3,8 @@ import accommodationController from "../controllers/accommodation.controller.js"
 
 const accommodationRouter = Router();
 
+// This route should be defined before /:id
+accommodationRouter.get("/search", accommodationController.search);
 accommodationRouter.get("/:id", accommodationController.getAccommodationDetail);
 
 export default accommodationRouter;
