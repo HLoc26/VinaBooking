5aafbff7dbc65f46f13c95d4aa5f8854ef417624
HLoc26 <dhl26052004@gmail.com>
2025-04-23 01:16:39 +0700

    feat: Add accommodation search.
    The search is based on location (city, state, postalCode, country), startDate, endDate

--- API/src/services/accommodation.service.js
+++ API/src/services/accommodation.service.js
@@ -1,4 +1,7 @@
 import { Accommodation, AccommodationAmenity, Address, Image, Room, RoomAmenity, Amenity } from "../database/models/index.js";
+import accommodationRepo from "../database/repository/accommodation.repo.js";
+import bookingRepo from "../database/repository/booking.repo.js";
+import roomRepo from "../database/repository/room.repo.js";
 
 export default {
 	async findById(id) {
@@ -67,5 +70,28 @@ export default {
 		});
 
 		return plain;
+	},
+
+	async search({ city, state, postalCode, country, startDate, endDate, roomCount, adultCount }) {
+		// 1. Find all rooms that are not available from startDate to endDate (status != CANCELED)
+		const bookedRooms = await bookingRepo.findBetweenDate(startDate, endDate);
+		const bookedRoomsIds = bookedRooms.map((room) => +room.roomId);
+
+		// 2. Find all accommodation in the location and their rooms
+		const matchedAccomm = await accommodationRepo.findByAddress({ city, state, postalCode, country });
+
+		// 3. Filter all the accommodation's rooms to select all the available rooms
+		const matchedAccommWithRooms = await Promise.all(
+			matchedAccomm.map(async (accomm) => {
+				const plainAccomm = accomm.get({ plain: true });
+				const accommId = accomm.id;
+				const rooms = (await roomRepo.findByAccommodationId(accommId))
+					.filter((room) => !bookedRoomsIds.includes(room.id)) // Get room that are available
+					.map((room) => room.get({ plain: true })); // Convert to plain object
+				return { ...plainAccomm, rooms: rooms };
+			})
+		);
+
+		return matchedAccommWithRooms;
 	},
 };
