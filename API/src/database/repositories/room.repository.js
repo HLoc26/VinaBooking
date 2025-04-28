3785f6ac3d4bae00a3eaf5c7155362b9453a15fc
HLoc26 <dhl26052004@gmail.com>
2025-04-28 11:48:42 +0700

    feat: Implement methods to check for room availability

--- API/src/database/repositories/room.repository.js
+++ API/src/database/repositories/room.repository.js
@@ -1,7 +1,36 @@
-import { Room } from "../models/index.js";
+import { Room as RoomModel, Booking as BookingModel } from "../models/index.js";
+import { Op } from "sequelize";
 
 export const RoomRepository = {
 	async findByAccommodationId(accommId) {
-		return await Room.findAll({ where: { accommodation_id: accommId } });
+		return await RoomModel.findAll({ where: { accommodationId: accommId } });
+	},
+
+	async findById(roomId) {
+		return await RoomModel.findOne({ where: { id: roomId } });
+	},
+
+	async isEmptyBetween(roomId, startDate, endDate) {
+		const bookings = await BookingModel.findOne({
+			where: {
+				roomId,
+				status: { [Op.ne]: "CANCELED" }, // Exclude canceled bookings
+				[Op.or]: [
+					{
+						startDate: { [Op.between]: [startDate, endDate] },
+					},
+					{
+						endDate: { [Op.between]: [startDate, endDate] },
+					},
+					{
+						startDate: { [Op.lte]: startDate },
+						endDate: { [Op.gte]: endDate },
+					},
+				],
+			},
+		});
+
+		// If no bookings are found, the room is empty
+		return !bookings;
 	},
 };
