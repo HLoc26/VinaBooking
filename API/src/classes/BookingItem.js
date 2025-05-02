3d546ec03691efa993cf874df7efd9aba04dafe1
HLoc26 <dhl26052004@gmail.com>
2025-05-02 12:36:22 +0700

    feat: implement save method in BookingItem class for database persistence

--- API/src/classes/BookingItem.js
+++ API/src/classes/BookingItem.js
@@ -1,3 +1,4 @@
+import { BookingRepository } from "../database/repositories/booking.repository.js";
 /**
  * Contains information about a room in a booking
  * @class BookingItem
@@ -7,6 +8,24 @@ class BookingItem {
 		this.id = id;
 		this.room = room;
 		this.count = count;
+	}
+
+	// Save the BookingItem instance to the database
+	async save(bookingId) {
+		if (!bookingId) {
+			throw new Error("Booking ID is required to save a BookingItem");
+		}
+
+		// Use the repository to save the BookingItem
+		const savedItem = await BookingRepository.createBookingItem({
+			bookingId,
+			roomId: this.room.id,
+			count: this.count,
+		});
+
+		// Update the instance with the saved ID
+		this.id = savedItem.id;
+		return this;
 	}
 }
 
