1f14e192a3124204b48e8762d4a01ffec9a22248
HLoc26 <dhl26052004@gmail.com>
2025-05-04 02:18:04 +0700

    refactor: Update Accommodation class to include amenities in constructor and enhance fromModel method

--- API/src/classes/Accommodation.js
+++ API/src/classes/Accommodation.js
@@ -1,25 +1,29 @@
 import { RoomRepository } from "../database/repositories/room.repository.js";
+import Address from "./Address.js";
 import Room from "./Room.js";
+import AccommodationAmenity from "./AccommodationAmenity.js";
 
 /**
  * Accommodation class
  * @class Accommodation
  */
 class Accommodation {
-	constructor({ id, name, address, rooms = [], isActive = true }) {
+	constructor({ id, name, address, amenities = [], rooms = [], isActive = true }) {
 		this.id = id;
 		this.name = name;
 		this.address = address;
+		this.amenities = amenities; // Amenity object
 		this.rooms = rooms; // Array of Room objects
 		this.isActive = isActive;
 	}
 
 	static fromModel(model) {
 		return new Accommodation({
-			id: model.id,
+			id: model.accommodationId || model.id,
 			name: model.name,
-			address: model.address,
-			rooms: model.rooms,
+			address: Address.fromModel(model.Address),
+			amenities: model.AccommodationAmenities?.map((amenity) => AccommodationAmenity.fromModel(amenity)) || [],
+			rooms: model.rooms?.map((room) => Room.fromModel(room)) || [],
 			isActive: model.isActive,
 		});
 	}
@@ -43,6 +47,25 @@ class Accommodation {
 
 	getRoomCount() {
 		return this.rooms.length;
+	}
+
+	getLocationString() {
+		return this.address.getLocationString();
+	}
+
+	getSimplifiedAmenities() {
+		return this.amenities?.map((amenity) => amenity.name).filter(Boolean) || [];
+	}
+
+	toPlain() {
+		return {
+			id: this.id,
+			name: this.name,
+			address: this.getLocationString(),
+			amenities: this.getSimplifiedAmenities(),
+			rooms: this.rooms.map((room) => room.toPlain()),
+			isActive: this.isActive,
+		};
 	}
 
 	toPlainWithRooms(rooms) {
