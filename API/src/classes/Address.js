56978dbe03c5eb882ed09b4c422a19ac955d7728
HLoc26 <dhl26052004@gmail.com>
2025-05-04 01:48:34 +0700

    refactor: Update Address class constructor to use object destructuring and enhance fromModel method

--- API/src/classes/Address.js
+++ API/src/classes/Address.js
@@ -3,7 +3,7 @@
  * @class Address
  */
 class Address {
-	constructor(id, street, city, state, postalCode, country, latitude, longitude) {
+	constructor({ id, street, city, state, postalCode, country, latitude, longitude }) {
 		this.id = id;
 		this.street = street;
 		this.city = city;
@@ -14,8 +14,25 @@ class Address {
 		this.longitude = longitude;
 	}
 
+	static fromModel(model) {
+		return new Address({
+			id: model.id,
+			street: model.street,
+			city: model.city,
+			state: model.state,
+			postalCode: model.postalCode,
+			country: model.country,
+			latitude: model.latitude,
+			longitude: model.longitude,
+		});
+	}
+
 	getCoordinates() {
 		return [this.latitude, this.longitude];
+	}
+
+	getLocationString() {
+		return `${this.city}, ${this.state}, ${this.country}`;
 	}
 }
 
