4257a50a59e3a5f9bbcfd45d52a8f2c8d53054ee
HLoc26 <dhl26052004@gmail.com>
2025-05-04 02:17:54 +0700

    refactor: Update Review class constructor to use object destructuring

--- API/src/classes/Review.js
+++ API/src/classes/Review.js
@@ -2,7 +2,7 @@
  * @class Review
  */
 class Review {
-	constructor(id, star, comment, images, reviewer, accommodation, reviewDate) {
+	constructor({ id, star, comment, images, reviewer, accommodation, reviewDate }) {
 		this.id = id;
 		this.star = star;
 		this.comment = comment;
@@ -10,6 +10,22 @@ class Review {
 		this.reviewer = reviewer;
 		this.accommodation = accommodation;
 		this.reviewDate = reviewDate;
+	}
+
+	static fromModel(model) {
+		return new Review({
+			id: model.id,
+			star: model.star,
+			comment: model.comment,
+			images: model.images,
+			reviewer: model.reviewer,
+			accommodation: model.accommodation,
+			reviewDate: model.reviewDate,
+		});
+	}
+
+	getStar() {
+		return +this.star;
 	}
 }
 
