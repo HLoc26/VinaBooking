5e7d0becd0530d70dace7b6258bbd0e8a838b656
zhwy512 <huyngh05@gmail.com>
2025-05-06 00:30:54 +0700

    feat: wrap App with Redux Provider and configure store

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./app/store";
+
 import Login from "./pages/Auth/Login/Login";
 import Register from "./pages/Auth/Register/Register";
 import Landing from "./pages/Landing/Landing";
@@ -7,18 +10,19 @@ import AccommodationDetail from "./pages/AccommodationDetail/AccommodationDetail
 
 function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route index element={<Landing />} />
-				<Route path="login" element={<Login />} />
-				<Route path="register" element={<Register />} />
-				<Route path="accommodation">
-					<Route path="detail/:aid" element={<AccommodationDetail />} /> {/* aid: Accommodation id */}
-				</Route>
-			</Routes>
-		</Router>
+		<Provider store={store}>
+			<Router>
+				<Routes>
+					<Route index element={<Landing />} />
+					<Route path="login" element={<Login />} />
+					<Route path="register" element={<Register />} />
+					<Route path="accommodation">
+						<Route path="detail/:aid" element={<AccommodationDetail />} /> {/* aid: Accommodation id */}
+					</Route>
+				</Routes>
+			</Router>
+		</Provider>
 	);
 }
 
 export default App;
-
