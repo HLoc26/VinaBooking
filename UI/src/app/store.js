1aa18ad2fb8a5a709482ee24348380f714aad9b9
zhwy512 <huyngh05@gmail.com>
2025-05-06 00:31:39 +0700

    feat: add favorites and booking reducers to Redux store

--- UI/src/app/store.js
+++ UI/src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
+import favoritesReducer from "../features/accommodationDetail/favoritesSlice";
+import bookingReducer from "../features/booking/bookingSlice";
 
 export default configureStore({
 	reducer: {
 		auth: authReducer,
+		favorites: favoritesReducer,
+		booking: bookingReducer,
 	},
 });
