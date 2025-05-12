fce916fdf426f6c7071a4a26e46fe3f33d4a7ea8
HLoc26 <dhl26052004@gmail.com>
2025-05-12 14:28:18 +0700

    feat: ask user to loggin before click on favourite

--- UI/src/components/ui/FavoriteButton/FavoriteButton.jsx
+++ UI/src/components/ui/FavoriteButton/FavoriteButton.jsx
@@ -1,4 +1,5 @@
-import { IconButton, Tooltip, CircularProgress } from "@mui/material";
+import { useState } from "react";
+import { IconButton, Tooltip, CircularProgress, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button } from "@mui/material";
 import { Favorite, FavoriteBorder } from "@mui/icons-material";
 import { useDispatch, useSelector } from "react-redux";
 import { addFavourite, removeFavourite } from "../../../features/favourite/favoritesSlice";
@@ -8,27 +9,55 @@ const FavoriteButton = ({ accommodation, onRemove, ...props }) => {
 
 	const favList = useSelector((state) => state.favourites.accomms);
 	const isLoadingGlobal = useSelector((state) => state.favourites.loading);
-
 	const isFavourite = favList.some((a) => a.id === accommodation.id);
 
+	const { isLoggedIn } = useSelector((state) => state.auth);
+
+	const [openLoginModal, setOpenLoginModal] = useState(false);
+
 	const handleClick = () => {
+		console.log(isLoggedIn);
+		if (!isLoggedIn) {
+			setOpenLoginModal(true);
+			return;
+		}
 		if (isLoadingGlobal) return;
+
 		if (isFavourite) {
 			dispatch(removeFavourite(accommodation));
-			onRemove();
+			onRemove?.(); // optional chaining
 		} else {
 			dispatch(addFavourite(accommodation));
 		}
 	};
 
+	const handleCloseModal = () => {
+		setOpenLoginModal(false);
+	};
+
 	return (
-		<Tooltip title={isFavourite ? "Remove from favorites" : "Add to favorites"} placement="top">
-			<span>
-				<IconButton onClick={handleClick} disabled={isLoadingGlobal} color={isFavourite ? "secondary" : "default"} {...props}>
-					{isLoadingGlobal ? <CircularProgress size={24} /> : isFavourite ? <Favorite /> : <FavoriteBorder />}
-				</IconButton>
-			</span>
-		</Tooltip>
+		<>
+			<Tooltip title={isFavourite ? "Remove from favorites" : "Add to favorites"} placement="top">
+				<span>
+					<IconButton onClick={handleClick} disabled={isLoadingGlobal} color={isFavourite ? "secondary" : "default"} {...props}>
+						{isLoadingGlobal ? <CircularProgress size={24} /> : isFavourite ? <Favorite /> : <FavoriteBorder />}
+					</IconButton>
+				</span>
+			</Tooltip>
+
+			<Dialog open={openLoginModal} onClose={handleCloseModal}>
+				<DialogTitle>Login Required</DialogTitle>
+				<DialogContent>
+					<DialogContentText>You need to be logged in to add this accommodation to your favorites.</DialogContentText>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleCloseModal}>Cancel</Button>
+					<Button onClick={() => (window.location.href = "/login")} variant="contained" color="primary">
+						Login
+					</Button>
+				</DialogActions>
+			</Dialog>
+		</>
 	);
 };
 
