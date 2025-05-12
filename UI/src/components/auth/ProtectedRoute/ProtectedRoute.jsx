2d34bb5176220d980896133a75b958375fc3d214
HLoc26 <dhl26052004@gmail.com>
2025-05-12 14:51:04 +0700

    feat: Return to original route after login

--- UI/src/components/auth/ProtectedRoute/ProtectedRoute.jsx
+++ UI/src/components/auth/ProtectedRoute/ProtectedRoute.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const ProtectedRoute = () => {
+	const location = useLocation();
 	const { user, isLoggedIn, loading } = useSelector((state) => state.auth);
 
 	// If authentication is still being checked, render nothing (or a loading indicator)
@@ -12,7 +13,7 @@ const ProtectedRoute = () => {
 
 	// If user is not logged in, redirect to login page
 	if (!isLoggedIn || !user) {
-		return <Navigate to="/login" replace />;
+		return <Navigate to="/login" replace state={{ from: location.pathname + location.search }} />;
 	}
 
 	// If user is logged in, render the child routes
