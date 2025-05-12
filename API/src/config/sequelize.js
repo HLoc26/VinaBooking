feea66b885159dba37248f46d48ef30dbd1ab8e4
HLoc26 <dhl26052004@gmail.com>
2025-05-12 21:32:24 +0700

    feat: set up connection to aiven mysql

--- API/src/config/sequelize.js
+++ API/src/config/sequelize.js
@@ -1,19 +1,16 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config";
 
+const HOST = process.env.MYSQL_HOST;
+const PORT = process.env.MYSQL_PORT;
 const DATABASE = process.env.MYSQL_DATABASE;
 const USERNAME = process.env.MYSQL_USER;
 const PASSWORD = process.env.MYSQL_PASSWORD;
 
 const sequelize = new Sequelize(DATABASE, USERNAME, PASSWORD, {
-	host: "db", // For docker, change to localhost if not using docker
+	host: HOST, // For docker, change to localhost if not using docker
+	port: PORT,
 	dialect: "mysql",
-	define: {
-		charset: "utf8mb4",
-		collate: "utf8mb4_unicode_ci",
-		underscored: true,
-		freezeTableName: true,
-	},
 	logging: false, // Change to true if want to check sql query
 });
 
