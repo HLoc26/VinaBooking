c68d0474c308276988b7cf32b232cfcbfb8d523b
JackieLOLz1 <trumkamatat@gmail.com>
2025-05-17 16:29:38 +0700

    Added a GET method to the booking routes to view bookings being protected by JWT-based authentication

--- API/src/routes/booking.routes.js
+++ API/src/routes/booking.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import bookingController from "../controllers/booking.controller.js";
+import authMiddleware from "../middlewares/auth.mdw.js";
 
 const bookingRouter = Router();
 
 bookingRouter.post("/", bookingController.bookRoom);
+bookingRouter.get("/", authMiddleware.decodeJwt, bookingController.viewBookings);
 
 export default bookingRouter;
