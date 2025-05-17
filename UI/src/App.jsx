e08d4cca9af6c3056a7492ebf14445fcdb525790
JackieLOLz1 <trumkamatat@gmail.com>
2025-05-17 16:36:27 +0700

    Actually implemented the BookingHistory page to the website

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -13,18 +13,12 @@ import ProtectedRoute from "./components/auth/ProtectedRoute/ProtectedRoute";
 import { restoreSession } from "./features/auth/authSlice";
 import BookRoom from "./pages/BookRoom/BookRoom";
 import Payment from "./pages/Payment/Payment";
+import BookingHistory from "./pages/BookingHistory";
 
 // Create these empty "under construction" pages
 const Profile = () => (
 	<div style={{ marginTop: "100px", textAlign: "center", padding: "20px" }}>
 		<h1>User Profile</h1>
-		<p>This page is under construction</p>
-	</div>
-);
-
-const Bookings = () => (
-	<div style={{ marginTop: "100px", textAlign: "center", padding: "20px" }}>
-		<h1>My Bookings</h1>
 		<p>This page is under construction</p>
 	</div>
 );
@@ -61,7 +55,7 @@ function App() {
 					<Route element={<ProtectedRoute />}>
 						<Route path="/saved" element={<FavoritesPage />} />
 						<Route path="/profile" element={<Profile />} />
-						<Route path="/bookings" element={<Bookings />} />
+						<Route path="/bookings" element={<BookingHistory />} />
 						<Route path="/settings" element={<Settings />} />
 						<Route path="/book" element={<BookRoom />} />
 						<Route path="/payment" element={<Payment />} />
