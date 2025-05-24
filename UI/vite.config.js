07045c512a4a73f4fd05a45888386a621a420e2d
HLoc26 <dhl26052004@gmail.com>
2025-05-24 15:05:50 +0700

    feat: Update vite config for npm run build

--- UI/vite.config.js
+++ UI/vite.config.js
@@ -3,17 +3,17 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: "/",
 	plugins: [react()],
+	build: {
+		outDir: "dist",
+	},
 	server: {
 		host: "0.0.0.0",
 		port: 5173,
 		strictPort: true,
 		watch: {
 			usePolling: true,
-		},
-		allowedHosts: ["vinabooking.com"],
-		proxy: {
-			"/uploads": "http://static:80",
 		},
 	},
 });
