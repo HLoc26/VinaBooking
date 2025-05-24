72f65147ac19d7fb8620ea9e5fb67e7379e266e6
HLoc26 <dhl26052004@gmail.com>
2025-05-25 04:03:48 +0700

    feat: Add notification adapters for email, SMS, and Zalo

---
+++ API/src/adapter/notificationAdapter.js
@@ -0,0 +1,37 @@
+import { NotificationService } from "../services/notification.service.js";
+
+class NotificationAdapter {
+	async send(identifier, otp) {
+		throw new Error("send() not implemented");
+	}
+}
+
+class EmailNotificationAdapter extends NotificationAdapter {
+	async send(identifier, otp) {
+		return NotificationService.sendOTPEmail(identifier, otp);
+	}
+}
+
+class SmsNotificationAdapter extends NotificationAdapter {
+	async send(identifier, otp) {
+		// Future implementation for SMS
+		// const phone = formatPhoneNumber(identifier)
+		// return NotificationService.sendOTPSMS(phone, otp)
+		throw new Error("SMS channel not implemented yet");
+	}
+}
+
+class ZaloNotificationAdapter extends NotificationAdapter {
+	async send(identifier, otp) {
+		// Future implementation for Zalo
+		// const zaloId = toZaloId(identifier)
+		// return NotificationService.sendOTPZalo(zaloId, otp)
+		throw new Error("Zalo channel not implemented yet");
+	}
+}
+
+export const NotificationAdapters = {
+	email: new EmailNotificationAdapter(),
+	sms: new SmsNotificationAdapter(),
+	zalo: new ZaloNotificationAdapter(),
+};
